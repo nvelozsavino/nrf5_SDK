3d84a4c890278edbfa527b95df86168d017fbade
Nicolas Veloz Savino <nicolas.veloz.savino@gmail.com>
2020-06-19 09:54:02 +0200

    Fixing unix file encodig to micro-ecc/build_all.sh
    Adding script to compile mirco-ecc selectively

--- external/micro-ecc/build.sh
+++ external/micro-ecc/build.sh
@@ -2,6 +2,22 @@
 
 # This script will use git (must be in $PATH) and arm-none-eabi tools in combination with GNU Make
 # to both fetch and compile all variants of micro-ecc for the nRF5 families
+if [ -z $1 ]; then
+  echo "you need to select an architecture/compiler. Ex: nrf52hf_armgcc"
+  exit
+fi
+
+if [ ! -d "$1" ]; then
+  echo "architecture/compiler $1 doesn't exit"
+  exit
+fi
+
+if [ ! -d "$1/armgcc" ]; then
+  echo "architecture/compiler $1/armgcc doesn't exit"
+  exit
+fi
+
+
 
 if ! [ -x "$(command -v git)" ]; then
     echo 'git is not installed. Please install and append to PATH.' >&2
@@ -13,12 +29,5 @@ if [ ! -f micro-ecc/uECC.c ]; then
     git clone https://github.com/kmackay/micro-ecc.git
 fi
 
-make -C nrf51_armgcc/armgcc &&
-make -C nrf51_iar/armgcc &&
-make -C nrf51_keil/armgcc &&
-make -C nrf52hf_armgcc/armgcc &&
-make -C nrf52hf_iar/armgcc &&
-make -C nrf52hf_keil/armgcc &&
-make -C nrf52nf_armgcc/armgcc &&
-make -C nrf52nf_iar/armgcc &&
-make -C nrf52nf_keil/armgcc
+make -C "$1/armgcc"
+
