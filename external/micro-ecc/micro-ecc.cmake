2e64f4d7ad94f49242db87c111dc6f87cbd025b3
Alejandro Veloz <aveloz@usb.ve>
2020-09-29 13:30:07 +0200

    Fixing micro-ecc build scripts for windows and adding cmake function

---
+++ external/micro-ecc/micro-ecc.cmake
@@ -0,0 +1,15 @@
+function (ADD_MICRO_ECC_TARGET)
+    if (WIN32 OR WIN64)
+        set(MICRO_ECC_BUILD build.bat)
+    else()
+        set(MICRO_ECC_BUILD ./build.sh)
+    endif()
+    set(MICRO_ECC_PATH ${CMAKE_CURRENT_FUNCTION_LIST_DIR})
+    add_custom_target(micro-ecc
+            COMMAND ${CMAKE_COMMAND} -E env "GNU_INSTALL_ROOT=${TOOLCHAIN_PREFIX}/" ${MICRO_ECC_BUILD} nrf52hf_armgcc
+            WORKING_DIRECTORY "${MICRO_ECC_PATH}"
+            BYPRODUCTS 	${MICRO_ECC_PATH}/nrf52hf_armgcc/armgcc/_build
+                        ${MICRO_ECC_PATH}/nrf52hf_armgcc/armgcc/micro_ecc_lib_nrf52.a
+                        ${MICRO_ECC_PATH}/micro-ecc
+            )
+endfunction()
