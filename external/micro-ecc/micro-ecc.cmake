1048171171a0024396ffb972a8745ab8edfad0ac
Alejandro Veloz <aveloz@usb.ve>
2020-09-29 13:34:35 +0200

    Simplifying micro-ecc cmake

--- external/micro-ecc/micro-ecc.cmake
+++ external/micro-ecc/micro-ecc.cmake
@@ -1,10 +1,10 @@
-function (ADD_MICRO_ECC_TARGET)
     if (WIN32 OR WIN64)
         set(MICRO_ECC_BUILD build.bat)
     else()
         set(MICRO_ECC_BUILD ./build.sh)
     endif()
-    set(MICRO_ECC_PATH ${CMAKE_CURRENT_FUNCTION_LIST_DIR})
+    set(MICRO_ECC_PATH ${CMAKE_CURRENT_LIST_DIR})
+    message(STATUS "MICRO_ECC ${MICRO_ECC_PATH}")
     add_custom_target(micro-ecc
             COMMAND ${CMAKE_COMMAND} -E env "GNU_INSTALL_ROOT=${TOOLCHAIN_PREFIX}/" ${MICRO_ECC_BUILD} nrf52hf_armgcc
             WORKING_DIRECTORY "${MICRO_ECC_PATH}"
@@ -12,4 +12,3 @@ function (ADD_MICRO_ECC_TARGET)
                         ${MICRO_ECC_PATH}/nrf52hf_armgcc/armgcc/micro_ecc_lib_nrf52.a
                         ${MICRO_ECC_PATH}/micro-ecc
             )
-endfunction()
